45e92b1f978315ec864a1da8563c8650d04403c9
xJam-byte <abylaimuratbayev@gmail.com>
2024-01-30 19:30:54 +0600

    register page

--- src/main/java/com/example/realclassproject/controller/AuthController.java
+++ src/main/java/com/example/realclassproject/controller/AuthController.java
@@ -1,13 +1,13 @@
-package com.example.realclassproject.controller.Auth;
+package com.example.realclassproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
+
     @GetMapping("/index")
     public String home(){
         return "index";
     }
-
 }
---
+++ src/main/java/com/example/realclassproject/controller/RegistrationController.java
@@ -0,0 +1,20 @@
+package com.example.realclassproject.controller;
+
+
+import com.example.realclassproject.dto.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class RegistrationController {
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+
+        return "registration";
+    }
+}
